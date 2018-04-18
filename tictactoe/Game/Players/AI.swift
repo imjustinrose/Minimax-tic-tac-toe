41bc3612b0a07bec16344cf2cbba20673f2702cf
imjustinrose <justin@justncode.com>
2018-04-18 19:49:46 -0400

    Add depth parameter to increase searching speed

--- tictactoe/Game/Players/AI.swift
+++ tictactoe/Game/Players/AI.swift
@@ -7,40 +7,41 @@
 //
 
 class AI: Player {
-    func maximize(_ newBoard: [Marker]) -> Int {
+    func maximize(_ newBoard: [Marker], with depth: Int) -> Int {
         var newBoard = newBoard
         var bestScore = -10_000
         
         for spot in board.availableSpots(of: newBoard) {
             newBoard[spot] = .ai
-            bestScore = max(bestScore, minimax(newBoard, player: .human))
+            bestScore = max(bestScore, minimax(newBoard, depth: depth-1, player: .human))
             newBoard[spot] = .none
         }
         
         return bestScore
     }
     
-    func minimize(_ newBoard: [Marker]) -> Int {
+    func minimize(_ newBoard: [Marker], with depth: Int) -> Int {
         var newBoard = newBoard
         var bestScore = 10_000
         
         for spot in board.availableSpots(of: newBoard) {
             newBoard[spot] = .human
-            bestScore = min(bestScore, minimax(newBoard, player: .ai))
+            bestScore = min(bestScore, minimax(newBoard, depth: depth-1, player: .ai))
             newBoard[spot] = .none
         }
         
         return bestScore
     }
     
-    func minimax(_ newBoard: [Marker], player: PlayerType) -> Int {
+    func minimax(_ newBoard: [Marker], depth: Int, player: PlayerType) -> Int {
         let spots = board.availableSpots(of: newBoard)
         
         if board.winner(is: .ai, of: newBoard) { return 10 }
         if board.winner(is: .human, of: newBoard) { return -10 }
+        if depth == 0 { return 0 }
         guard !spots.isEmpty else { return 0 }
         
-        return player == .ai ? maximize(newBoard) : minimize(newBoard)
+        return player == .ai ? maximize(newBoard, with: depth) : minimize(newBoard, with: depth)
     }
     
     func findBestMove(_ newBoard: [Marker]) -> Int {
@@ -49,7 +50,7 @@ class AI: Player {
         
         for spot in board.availableSpots(of: newBoard) {
             newBoard[spot] = .ai
-            let moveValue = minimax(newBoard, player: .human)
+            let moveValue = minimax(newBoard, depth: 5, player: .human)
             newBoard[spot] = .none
             
             if moveValue > optimal.value {
